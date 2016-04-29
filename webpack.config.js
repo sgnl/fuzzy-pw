4c68946df37223792d926e9f25cb9d203c94e317
sgnl <rayrfarias@gmail.com>
2016-04-28 16:13:26 -1000

    adds babel, webpack, and other magics

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: './src/index',
+  output: {
+    filename: 'public/js/browser-bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+    ]
+  }
+};
